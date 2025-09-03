32ae83eeb2a6c41edf5bb40f850167c0384ab2d8
Victor Soares da Silveira <05842851164@ifb.local>
2025-09-03 10:28:19 -0300

    reverter arquivo1

--- pasta1/arquivo1.py
+++ pasta1/arquivo1.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #Pedro Reverência
+=======
+#Thigas
+>>>>>>> parent of efc0665 (alteração arquivo1 para arrasca)
 =======
 #Thigas
 >>>>>>> parent of efc0665 (alteração arquivo1 para arrasca)
