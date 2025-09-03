8e69010234d0b97976cdc2361baf3cb51dfce2b9
Victor Soares da Silveira <05842851164@ifb.local>
2025-09-03 10:32:13 -0300

    rei da america

--- pasta1/arquivo1.py
+++ pasta1/arquivo1.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#Pedro Reverência
-=======
-#Thigas
->>>>>>> parent of efc0665 (alteração arquivo1 para arrasca)
-=======
-#Thigas
->>>>>>> parent of efc0665 (alteração arquivo1 para arrasca)
+#Rei da América
